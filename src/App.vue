91282ea983a002ac685c8a68f9ce1f6fc43334bd
kaiwen <wenkai169@163.com>
2022-12-13 19:28:14 +0800

    update brand icon

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <v-app>
     <v-main>
-      <v-toolbar border color="white">
+      <v-toolbar border color="white" title="superK's Blog">
         <template v-slot:prepend>
-          <a href="/">
-          <v-img width="220" src="/public/logo2.png"></v-img>
-        </a>
+          <a href="/" class="dishref">
+            <v-icon icon="mdi-star" color="#ff9000" size="x-large" class="ml-3"></v-icon>
+          </a>
         </template>
       </v-toolbar>
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
+
+<style scoped>
+.dishref {
+  text-decoration: none;
+}
+</style>
